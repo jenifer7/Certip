889a3f72c59b5dcbf1ecea306246887042ef8cc8
jenifer7 <jeni9jacinto7p@gmail.com>
2019-10-20 01:13:45 -0600

    setting migration customers

--- database/migrations/2019_10_20_054802_create_customers_table.php
+++ database/migrations/2019_10_20_054802_create_customers_table.php
@@ -15,6 +15,10 @@ class CreateCustomersTable extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('fullname');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('nit');
             $table->timestamps();
         });
     }
