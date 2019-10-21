77bcc17985f31bba24d43cad2a3a510c8feea7cf
jenifer7 <jeni9jacinto7p@gmail.com>
2019-10-21 09:21:11 -0600

    Added protected fillable Customer

--- app/Customer.php
+++ app/Customer.php
@@ -4,7 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * La clase Customer se utiliza para llevar registro de clientes e
+ * ingresar nuevos
+*/
 class Customer extends Model
 {
-    //
+    protected $fillable = [
+        'fullname',
+        'phone',
+        'address',
+        'nit'
+    ];
 }
