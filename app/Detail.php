d0e0a5eab64bda5b21e9f1aeafb8411f3afb7b28
jenifer7 <jeni9jacinto7p@gmail.com>
2019-10-21 09:21:29 -0600

    Added protected fillable detail

--- app/Detail.php
+++ app/Detail.php
@@ -4,7 +4,18 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * El modelo de detalle nos ayuda con informcion mas amplia 
+ * de cada venta realizada
+ */
 class Detail extends Model
 {
-    //
+    protected $fillable = [
+        'sale_id',
+        'product_id',
+        'price_unit',
+        'quantity',
+        'sub_total',
+        'total_sales'
+    ];
 }
