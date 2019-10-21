da69ea8d17c9c01cb6041ba40a6a4b6a91aa1b45
jenifer7 <jeni9jacinto7p@gmail.com>
2019-10-21 09:22:34 -0600

    Added protected fillable supplier

--- app/Supplier.php
+++ app/Supplier.php
@@ -4,7 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Registro de los proveedores 
+ */
 class Supplier extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'phone', 'address'
+    ];
 }
