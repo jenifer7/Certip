1afebefd5d631daf55037309a5cfe2fdf28cef55
jenifer7 <jeni9jacinto7p@gmail.com>
2019-10-21 22:40:24 -0600

    added seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
+        $this->call(EmployeesTableSeeder::class);
+        $this->call(SuppliersTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
+        $this->call(CustomersTableSeeder::class);
+        $this->call(SalesTableSeeder::class);
+        $this->call(DetailsTableSeeder::class);
     }
 }
