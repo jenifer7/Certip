9c70cfc10c64191ce049dc3ca08c9ca3c8768963
jenifer7 <jeni9jacinto7p@gmail.com>
2019-10-22 10:31:33 -0600

    modified model customer

--- app/Customer.php
+++ app/Customer.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Model;
 */
 class Customer extends Model
 {
+    protected $table = 'customers';
     protected $fillable = [
         'fullname',
         'phone',
