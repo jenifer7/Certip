044cf06878c7d9098df3c5a6e1d210166a117e4b
jenifer7 <jeni9jacinto7p@gmail.com>
2019-10-22 10:43:31 -0600

    modified model supplier

--- app/Supplier.php
+++ app/Supplier.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Supplier extends Model
 {
+    protected $table = 'suppliers';
     protected $fillable = [
         'name', 'phone', 'address'
     ];
