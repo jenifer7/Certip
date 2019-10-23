28e0fcce2d4708394ae32e5899ecdf32a072c9a5
jenifer7 <jeni9jacinto7p@gmail.com>
2019-10-23 11:23:39 -0600

    added new attribute

--- database/migrations/2019_10_20_052457_create_suppliers_table.php
+++ database/migrations/2019_10_20_052457_create_suppliers_table.php
@@ -18,6 +18,7 @@ class CreateSuppliersTable extends Migration
             $table->string('name');
             $table->string('phone');
             $table->string('address');
+            $table->boolean('is_active')->default('1');
             $table->timestamps();
         });
     }
