024bb8b453d2841526b4fef3a7ea514e4f8ae17f
jenifer7 <jeni9jacinto7p@gmail.com>
2019-10-29 16:31:01 -0600

    added view index product

---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.layout')
+
+@section('title', 'Bienvenido!!')
+@section('content')
+
+<div>
+    <table class="table">
+        <thead>
+            <tr>
+                <td>Id</td>
+                <td>Nombre Producto</td>
+                <td>Precio Unidad</td>
+                <td>Stock</td>
+                <td>Proveedor</td>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($prod as $prod)
+            <tr>
+                <td>{{ $prod->id }}</td>
+                <td>{{ $prod->name }}</td>
+                <td>{{ $prod->unit_price }}</td>
+                <td>{{ $prod->stock }}</td>
+                <td>{{ $prod->supplier_id }}</td>
+                <td><a href="">Detalle</a></td>
+            </tr>
+           @endforeach
+        </tbody>
+    </table>
+</div>
+
+@endsection
