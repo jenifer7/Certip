13ded729a4d31de1b4a5894a7f011adc2a02297a
jenifer7 <jeni9jacinto7p@gmail.com>
2019-10-29 10:26:55 -0600

    the view index was added to show the list of registered employees

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -1,10 +1,36 @@
 @extends('layouts.layout')
 
-@section('title','Usuario')
+@section('title', 'Bienvenido!!')
 @section('content')
 
 <div>
-    
+    <table class="table">
+        <thead>
+            <tr>
+                <td>Id</td>
+                <td>Nombre Completo</td>
+                <td>Posición</td>
+                <td>Teléfono</td>
+                <td>Direccion</td>
+                <td>Género</td>
+                <td>Estado</td>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($emple as $emple)
+            <tr>
+                <td>{{ $emple->id }}</td>
+                <td>{{ $emple->fullname }}</td>
+                <td>{{ $emple->position }}</td>
+                <td>{{ $emple->phone }}</td>
+                <td>{{ $emple->address }}</td>
+                <td>{{ $emple->gender }}</td>
+                <td>{{ $emple->is_active }}</td>
+                <td><a href="{{ route('emplea.show', $emple->id) }}">Detalle</a></td>
+            </tr>
+           @endforeach
+        </tbody>
+    </table>
 </div>
 
 @endsection
