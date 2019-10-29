f2315690aa4bf35b464677778081eedbac5eea40
jenifer7 <jeni9jacinto7p@gmail.com>
2019-10-29 10:22:36 -0600

    modified view show customer

---
+++ resources/views/customer/show.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.layout')
+@section('title','Bienvenido!')
+
+@section('content')
+<style>
+    .hero {
+        border-radius: 3px;
+    }
+</style>
+<div class="hero is-link">
+    <div class="hero-body">
+        <div class="container">
+            <h1 class="title is-1">Proveedor</h1>
+        </div>
+    </div>
+</div>
+<div class="notification">
+    <table class="table is-fullwidth">
+        <thead>
+            <tr>
+                <th>Nombre Completo</th>
+                <th>Telefono</th>
+                <th>Dirección</th>
+                <th>NIT</th>
+                <th>Estado</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>{{ $custom->fullname }}</td>
+                <td>{{ $custom->phone }}</td>
+                <td>{{ $custom->address }}</td>
+                <td>{{ $custom->nit }}</td>
+                <td>{{ $custom->is_active }}</td>
+                <td><a href="{{ route('cliente.create') }}"><input type="submit" value="Agregar Nuevo"></a></td>
+                <td><a href="{{ route('cliente.edit', $custom->id) }}"><input type="submit" value="Editar"></a></td>
+                <td>
+                    <form action="{{ route('cliente.destroy', $custom->id) }}" method="post"> 
+                        @csrf
+                        @method('DELETE')
+                        <input type="submit" value="Borrar">
+                    </form>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+@endsection
