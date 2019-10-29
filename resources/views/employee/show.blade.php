c178e1d64ec6204369af451054594115c7d79676
jenifer7 <jeni9jacinto7p@gmail.com>
2019-10-29 10:28:49 -0600

    added view shows full employee information

---
+++ resources/views/employee/show.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.layout')
+@section('title','Bienvenido!')
+
+@section('content')
+<style>
+    .hero {
+        border-radius: 3px;
+    }
+</style>
+<div class="hero is-link">
+    <div class="hero-body">
+        <div class="container">
+            <h1 class="title is-1">Proveedor</h1>
+        </div>
+    </div>
+</div>
+<div class="notification">
+    <table class="table is-fullwidth">
+        <thead>
+            <tr>
+                <th>Nombre Completo</th>
+                <th>Posición</th>
+                <th>Teléfono</th>
+                <th>Direccion</th>
+                <th>Genero</th>
+                <th>Estado</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>{{ $emple->fullname }}</td>
+                <td>{{ $emple->position }}</td>
+                <td>{{ $emple->phone }}</td>
+                <td>{{ $emple->address }}</td>
+                <td>{{ $emple->gender }}</td>
+                <td>{{ $emple->is_active }}</td>
+                <td><a href="{{ route('emplea.create') }}"><input type="submit" value="Agregar Nuevo"></a></td>
+                <td><a href="{{ route('emplea.edit', $emple->id) }}"><input type="submit" value="Editar"></a></td>
+                <td>
+                    <form action="{{ route('emplea.destroy', $emple->id) }}" method="post"> 
+                        @csrf
+                        @method('DELETE')
+                        <input type="submit" value="Borrar">
+                    </form>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+@endsection
